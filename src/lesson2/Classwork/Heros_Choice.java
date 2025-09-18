066206294aea08145376183b95fc7b6684fd5ca1
muradpenah <murad.panahaliyev@gmail.com>
2025-09-18 19:16:20 +0400

    Commit

--- src/lesson2/Classwork/Heros_Choice.java
+++ src/lesson2/Classwork/Heros_Choice.java
@@ -7,15 +7,13 @@ public class Heros_Choice {
         Scanner sc = new Scanner(System.in);
         System.out.println("qehreamn sechimini reqemle edin: ");
         int a = sc.nextInt();
-        String var10000;
-        switch (a) {
-            case 1 -> var10000 = "Zeus";
-            case 2 -> var10000 = "Poseidon";
-            case 3 -> var10000 = "Kratos";
-            default -> var10000 = "Invalid";
-        }
+        String result = switch (a) {
+            case 1 ->  "Zeus";
+            case 2 ->  "Poseidon";
+            case 3 ->  "Kratos";
+            default ->  "Invalid";
+        };
 
-        String result = var10000;
         System.out.println(result);
     }
 }
