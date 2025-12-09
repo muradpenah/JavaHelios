ab5f579780dc33aeee38c113195f8fa6b1b0f836
muradpenah <murad.panahaliyev@gmail.com>
2025-12-09 18:46:12 +0400

    Lesson 20 Lambda Expression task hell olundu

---
+++ src/lesson20_Lambda/task2/ConsumerMain.java
@@ -0,0 +1,14 @@
+package lesson20_Lambda.task2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Consumer {
+    public static void main(String[] args) {
+        List<String> sozler = new ArrayList<>();
+        sozler.add("Salam");
+        sozler.add("Lambda");
+        sozler.add("Java");
+        
+    }
+}
